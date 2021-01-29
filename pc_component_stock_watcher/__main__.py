fc4e675313df70526af7f9a548c99353043cda4a
Emil Rekola <emil.rekola@hotmail.com>
2021-01-29 11:31:41 +0200

    Added starting files for the project

---
+++ pc_component_stock_watcher/__main__.py
@@ -0,0 +1,5 @@
+import sys
+from pc_component_stock_watcher.run import main
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ pc_component_stock_watcher/run.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+
+def main():
+    print(f"Hello world, this is {Path(__file__).name}")
